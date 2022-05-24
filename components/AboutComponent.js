43ae4acff566abf4f74331d21c0fd196b755f5f0
Ben Jacobs <bencjacobs6@gmail.com>
2022-05-24 12:55:39 -0400

    Animatable

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,75 +4,70 @@ import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
   return {
-      partners: state.partners
+    partners: state.partners
   };
 };
 
-function Mission () {
+function Mission() {
   return (
     <Card title='Our Mission'>
-    <Text style={{margin: 10}}>
-    We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness.
-    We increase access to adventure for the public while promoting safe and respectful use of resources. 
-    The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. 
-    We also present a platform for campers to share reviews on campsites they have visited with each other.
-    </Text>
-  </Card>
+      <Text style={{ margin: 10 }}>
+        We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness.
+        We increase access to adventure for the public while promoting safe and respectful use of resources.
+        The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards.
+        We also present a platform for campers to share reviews on campsites they have visited with each other.
+      </Text>
+    </Card>
   )
 }
 
 export class About extends Component {
 
-    static navigationOptions = {
-        title: 'AboutUs'
-    }
+  static navigationOptions = {
+    title: 'AboutUs'
+  }
 
   render() {
-    const renderPartner = ({item}) => {
+    const renderPartner = ({ item }) => {
       return (
-          <ListItem
-              title={item.name}
-              subtitle={item.description}
-              leftAvatar={{source: {uri: baseUrl + item.image}}}
-          />
+        <ListItem
+          title={item.name}
+          subtitle={item.description}
+          leftAvatar={{ source: { uri: baseUrl + item.image } }}
+        />
       );
-  };
+    };
 
-  if (this.props.partners.isLoading) {
+    if (this.props.partners.errMess) {
       return (
-          <ScrollView>
-              <Mission />
-              <Card
-                  title='Community Partners'>
-                  <Loading />
-              </Card>
-          </ScrollView>
+        <ScrollView>
+          <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+            <Mission />
+            <Card
+              title="Community Partners">
+              <Text>{this.props.partners.errMess}</Text>
+            </Card>
+          </Animatable.View>
+        </ScrollView>
       );
-  }
-  if (this.props.partners.errMess) {
-      return (
-          <ScrollView>
-              <Mission />
-              <Card
-                  title='Community Partners'>
-                  <Text>{this.props.partners.errMess}</Text>
-              </Card>
-          </ScrollView>
-      );
-  }
+    }
     return (
       <ScrollView>
-            <Mission/>
-            <Card title='Community Partners'>
+        <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+          <Mission />
+          <Card
+            title="Community Partners">
             <FlatList
-                data={this.props.partners.partners}
-                renderItem={renderPartner}
-                keyExtractor={item => item.id.toString()}
+              data={this.props.partners.partners}
+              renderItem={renderPartner}
+              keyExtractor={item => item.id.toString()}
             />
-            </Card>
+          </Card>
+        </Animatable.View>
       </ScrollView>
     )
   }
